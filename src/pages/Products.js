07f730cdbe0ff7fad17f89fff4ef0ef3244d183c
moin2672 <moin2672@gmail.com>
2023-02-06 15:55:29 +0530

    4. adding links

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,5 +1,12 @@
+import { Link } from 'react-router-dom';
+
 function ProductsPage() {
-  return <h1>The Products Page</h1>;
+  return <div>
+  <h1>The Products Page</h1>
+  <p>
+        Go to <Link to="/">Home</Link>.
+      </p>
+  </div>
 }
 
 export default ProductsPage;
