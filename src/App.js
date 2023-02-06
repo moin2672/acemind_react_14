659429b30f95d22161dfd294fea00fafc506994d
moin2672 <moin2672@gmail.com>
2023-02-06 15:48:01 +0530

    1a. Route Setup

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomePage from './pages/Home';
 
 const router = createBrowserRouter([
   { path: '/', element: <HomePage /> },
+  { path: '/products', element: <h1>Products</h1> },
 ]);
 
 function App() {
